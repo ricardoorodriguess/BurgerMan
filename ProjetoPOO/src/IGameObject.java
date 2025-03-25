432dbbe02c568d39ed03e21d32d5116636811160
rodrigolinhas <rodrigolinhas18@gmail.com>
2025-03-25 18:44:39 +0000

    ADDED INTERFACES (TODO IMPLEMENTATION)

---
+++ ProjetoPOO/src/IGameObject.java
@@ -0,0 +1,15 @@
+public interface IGameObject {
+    /**
+     * @return the name of the GameObject
+     */
+    String name();
+    /**
+     * @return the Transform of the GameObject
+     */
+    ITransform transform();
+    /**
+     * @return the Collider of the GameObject with its centroid at this.transform().position()
+     */
+    ICollider collider();
+    // …
+}
