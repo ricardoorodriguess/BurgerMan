0fccf262d7097fe40275cd59bc49feaaa7374012
Ricardo Rodrigues <pauloroseigues@gmail.com>
2025-03-26 14:48:22 +0000

    ADDED Point class methods with doc

--- ProjetoPOO/src/Point.java
+++ ProjetoPOO/src/Point.java
@@ -1,21 +1,46 @@
-import java.awt.geom.Point2D;
-import java.lang.Cloneable;
-import java.io.Serializable;
+import static java.lang.Math.*;
 
-public class Point extends Point2D
+/**
+ * This class represents a Point in 2D.
+ * @author Ricardo Rodrigues
+ * @author Rodrigo Linhas
+ * @author Tiago Tome
+ * @version March 26, 2025
+ */
+public class Point
 {
     private double x;
     private double y;
 
-    @Override
-    public double getX() {return this.x;}
+    /**
+     * Returns the abscissa of the Point.
+     * @return x -> abscissa of a Point
+     */
+    public double getX() { return this.x; }
 
-    @Override
-    public double getY() {return this.y;}
+    /**
+     * Returns the ordinate of the Point.
+     * @return y -> ordinate of a Point
+     */
+    public double getY() { return this.y; }
 
-    @Override
-    public void setLocation(double x, double y)
+    /**
+     * Response method to do a translation in one Point.
+     * @param point, coordinates x and y to add to this.point
+     */
+    public void move(Point point)
     {
+        this.x += point.x;
+        this.y += point.y;
+    }
 
+    /**
+     * Response method to calculate distance between two Points.
+     * @param point, point to calculate distance
+     * @return distance between point and this.point
+     */
+    public double dist(Point point)
+    {
+        return sqrt(pow(this.x - point.x, 2) + pow(this.y - point.y, 2));
     }
 }
