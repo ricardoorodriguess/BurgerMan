2078a199c4818be63e50f0188170f87db3b1a6a1
Ricardo Rodrigues <pauloroseigues@gmail.com>
2025-04-04 20:10:01 +0100

    Added doc to GameEngine

--- ProjetoPOO/src/GameEngine.java
+++ ProjetoPOO/src/GameEngine.java
@@ -1,17 +1,35 @@
 import java.util.ArrayList;
 
+/**
+ * This class represent a various gameObjects in an arrayList
+ * @author Ricardo Rodrigues
+ * @author Rodrigo Linhas
+ * @author Tiago Tome
+ * @version March 27, 2025
+ */
 public class GameEngine {
     private final ArrayList<GameObject> loadedObjects;
 
+    /**
+     * Construtor of GameEngine
+     */
     public GameEngine() {
-        this.loadedObjects = new ArrayList<>();
+        this.loadedObjects = new ArrayList<GameObject>();
     }
 
-    public void add(GameObject go) {
-        loadedObjects.add(go);
+    /**
+     * Method do add gameObjects to arrayList
+     * @param gameObject
+     */
+    public void add(GameObject gameObject) {
+        loadedObjects.add(gameObject);
     }
 
-    public void destroy(GameObject go) {
-        loadedObjects.remove(go);
+    /**
+     * Method do remove gameObjects from arrayList
+     * @param gameObject
+     */
+    public void destroy(GameObject gameObject) {
+        loadedObjects.remove(gameObject);
     }
 }
