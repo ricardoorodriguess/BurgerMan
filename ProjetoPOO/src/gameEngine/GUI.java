b703c1a12605c4369b5630a10dc4d76f5df5d273
Rodrigo Linhas <rodrigolinhas18@gmail.com>
2025-05-17 19:11:55 +0100

    javadoc GUI

--- ProjetoPOO/src/gameEngine/GUI.java
+++ ProjetoPOO/src/gameEngine/GUI.java
@@ -3,55 +3,120 @@ package gameEngine;
 import gameEngine.object.IGameObject;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
+import javax.swing.*;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+/**
+ * This class represents the GUI of the game Burger Man
+ * @author Ricardo Rodrigues
+ * @author Rodrigo Linhas
+ * @author Tiago Tome
+ * @version May 17, 2025
+ */
 public class GUI implements IGUI {
     private final CopyOnWriteArrayList<InputEvent> queue;
 
+    /**
+     * Constructor of the GUI class.
+     * Initializes the input event queue using CopyOnWriteArrayList
+     * to ensure thread safety in concurrent environments.
+     */
     public GUI() {
         queue = new CopyOnWriteArrayList<>();
     }
 
+     /**
+      * Removes the next event from the input queue, if it exists.
+      * @return next InputEvent in queue or null if the queue is empty.
+      */
     @Override
     public @Nullable InputEvent dequeue() {
         return queue.removeFirst();
     }
 
+    /**
+     * Displays the game objects in the GUI.
+     * @param list List of game objects to be displayed.
+     */
     @Override
     public void display(@NotNull List<IGameObject> list) {
-        // TODO
+        // Exemplo
+        //JFrame frame = new JFrame("Burger Man");
+        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.setSize(800, 600);
+        //frame.setResizable(false);
+        //frame.setVisible(true);
+        //TODO logica do jogo
     }
 
+    /**
+     * Handles the keyTyped event. Currently, this method does not perform any action.
+     * @param e KeyEvent containing information about the key typed.
+     */
     @Override
     public void keyTyped(KeyEvent e) {}
 
+    /**
+     * Handles the keyPressed event.
+     * Adds the KeyEvent to the input queue.
+     * @param e KeyEvent containing information about the key pressed.
+     */
     @Override
     public void keyPressed(KeyEvent e) {
         queue.addLast(e);
     }
 
+    /**
+     * Handles the keyReleased event.
+     * Currently, this method does not perform any action.
+     * @param e KeyEvent containing information about the key released.
+     */
     @Override
     public void keyReleased(KeyEvent e) {}
 
+    /**
+     * Handles the mouseClicked event.
+     * Adds the MouseEvent to the input queue.
+     * @param e MouseEvent containing information about the mouse click.
+     */
     @Override
     public void mouseClicked(MouseEvent e) {
         queue.addLast(e);
     }
 
+    /**
+     * Handles the mousePressed event.
+     * Currently, this method does not perform any action.
+     * @param e MouseEvent containing information about the mouse press.
+     */
     @Override
     public void mousePressed(MouseEvent e) {}
 
+    /**
+     * Handles the mouseReleased event.
+     * Currently, this method does not perform any action.
+     * @param e MouseEvent containing information about the mouse release.
+     */
     @Override
     public void mouseReleased(MouseEvent e) {}
 
+    /**
+     * Handles the mouseEntered event.
+     * Currently, this method does not perform any action.
+     * @param e MouseEvent containing information about the mouse entering the component.
+     */
     @Override
     public void mouseEntered(MouseEvent e) {}
 
+    /**
+     * Handles the mouseExited event.
+     * Currently, this method does not perform any action.
+     * @param e MouseEvent containing information about the mouse exiting the component.
+     */
     @Override
     public void mouseExited(MouseEvent e) {}
 }
--- ProjetoPOO/src/gameEngine/IGUI.java
+++ ProjetoPOO/src/gameEngine/IGUI.java
@@ -9,8 +9,20 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseListener;
 import java.util.List;
 
+/**
+ The {@code gameEngine.IGUI} interface defines the contract for graphical user interfaces
+ * within the game engine
+ * @author Ricardo Rodrigues
+ * @author Rodrigo Linhas
+ * @author Tiago Tome
+ * @version May 27, 2025
+ */
 public interface IGUI extends KeyListener, MouseListener {
     @Nullable InputEvent dequeue();
 
+    /**
+     * Displays the given list of game objects on the GUI.
+     * @param list the list of IGameObject instances to be displayed; must not be null
+     */
     void display(@NotNull List<IGameObject> list);
 }
