cc63e5eca63fcd3a1b0255e67afa2a055821aecc
Rodrigo Linhas <rodrigolinhas18@gmail.com>
2025-05-25 21:57:40 +0100

    undo previous commit

--- ProjetoPOO/src/gameEngine/behaviour/EnemyBehavior.java
+++ ProjetoPOO/src/gameEngine/behaviour/EnemyBehavior.java
@@ -115,7 +115,7 @@ public class EnemyBehavior extends Behaviour {
             switch (gameObject.name()) {
                 case "Pickle":
                     if ((c2 = gameObject.collider()) == null || !c2.isColliding(c1)) continue;
-                    Client.ENGINE.getPlayerObject().behaviour().onDisabled();
+                    ((PlayerBehaviour) Client.ENGINE.getPlayerObject().behaviour()).slowDown();
                     break;
                 case "Inter":
                     if ((c2 = gameObject.collider()) == null || !c2.isColliding(c1) || currentInter) continue;
